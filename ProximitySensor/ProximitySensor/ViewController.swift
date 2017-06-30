aeb9be9fa0012aba2fcce245c6304d2fc6f14343
Tobias Böttcher <ttw88@gmx.de>
2017-06-30 20:44:34 +0200

    count changes of proximity sensor

--- ProximitySensor/ProximitySensor/ViewController.swift
+++ ProximitySensor/ProximitySensor/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counter: UILabel!
+    
+    @IBAction func resetCounter(_ sender: UIButton) {
+        
+        counter.text = "0"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        activateProximitySensor()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    func proximityChanged(notification:Notification) -> Void {
+        print("Changed to: \(UIDevice.current.proximityState)")
+        
+        if UIDevice.current.proximityState == true  {
+            var count = Int(counter.text!)!
+            count = count + 1
+            counter.text = "\(count)"
+        }
+    }
+    
+    
+    func activateProximitySensor() {
+        let device = UIDevice.current
+        device.isProximityMonitoringEnabled = true
+        if device.isProximityMonitoringEnabled {
+            let nc = NotificationCenter.default
+            nc.addObserver(forName: NSNotification.Name.UIDeviceProximityStateDidChange, object: nil, queue: nil, using: proximityChanged)
+        }
+    }
+    
+    
 }
 
