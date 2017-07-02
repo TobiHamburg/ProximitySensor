acfa93869be9f933cb09fe2b7810e6ddd65d9597
Tobias Böttcher <ttw88@gmx.de>
2017-07-02 13:56:41 +0200

    challenge mode

--- ProximitySensor/ProximitySensor/CounterViewController.swift
+++ ProximitySensor/ProximitySensor/CounterViewController.swift
@@ -13,10 +13,13 @@ import FirebaseAuth
 class CounterViewController: UIViewController {
     
     let ref = Database.database().reference(withPath: "users")
+    let refChallenge = Database.database().reference(withPath: "challenge")
     var user: MyUser!
-    var usersRef = Database.database().reference(withPath: "online")
     
+    @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var counter: UILabel!
+    @IBOutlet weak var challengePartnerNameLabel: UILabel!
+    @IBOutlet weak var challengePartnerCounterLabel: UILabel!
     
     @IBAction func resetCounter(_ sender: UIButton) {
         
@@ -25,12 +28,55 @@ class CounterViewController: UIViewController {
     
     @IBAction func startChallengeDidTouch(_ sender: UIButton) {
         
+        let alert = UIAlertController(title: "Begin Challenge",
+                                      message: "Type a friends name",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Do it!",
+                                       style: .default) { action in
+                                        
+                                        
+                                        print("your challenge partner: \(alert.textFields![0].text!)")
+                                        let challengeParterName = alert.textFields![0].text!
+                                        self.retrieveDataFromUser(name: challengeParterName)
+                                        
+                                        }
+        
+        alert.addTextField { textChallengePartner in
+            textChallengePartner.placeholder = "Username"
+        }
+
+        
+        alert.addAction(saveAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func retrieveDataFromUser(name: String) {
+        print("retrieveDataFromUser: \(name)")
+        
+        self.challengePartnerNameLabel.text = name
+        self.challengePartnerNameLabel.isHidden = false
+        self.challengePartnerCounterLabel.text = "0"
+        self.challengePartnerCounterLabel.isHidden = false
+        
+        self.refChallenge.child(name).observe(.value, with: { snapshot in
+        
+            if snapshot.exists() {
+                let dictionary: [String: Int] = (snapshot.value as! NSDictionary) as! [String : Int]
+                
+                self.challengePartnerCounterLabel.text = "\(dictionary["count"] ?? 0)"
+            } else {
+                self.challengePartnerCounterLabel.text = "0"
+            }
+        })
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("ViewController - viewDidLoad")
+        self.hideKeyboardWhenTappedAround()
         activateProximitySensor()
         
         Auth.auth().addStateDidChangeListener { auth, user in
@@ -53,6 +99,10 @@ class CounterViewController: UIViewController {
             
             user.count = count
             self.ref.setValue(user.toAnyObject())
+            self.refChallenge.child(userName.text!).setValue([
+                "count" : count
+                ]
+)
         }
     }
     
@@ -69,5 +119,18 @@ class CounterViewController: UIViewController {
     }
     
     
+}
+
+// Put this piece of code anywhere you like
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
 
